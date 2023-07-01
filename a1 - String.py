db1ed3641aec167f6b64077dbad552b0caaae39c
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-01 22:25:02 +0700

    just string

---
+++ a1 - String.py
@@ -0,0 +1,44 @@
+# String
+
+# Single Quote
+user = 'Hello World!'
+print(user)
+
+# Double Quote
+user = "Hello World!"
+print(user)
+
+# Quotes
+print('"Hello World!"')
+print("'Hello World!'")
+print("It's 9 o'clock!")
+
+# Backslash
+print('It\'s 9 o\'clock!')
+print("C:\\User\\asus\\Program Files")
+
+# Tab
+print("Purwokerto \tJakarta")
+
+# Backspace
+print("JKT \b48")
+
+# Newline
+print("Hello \nWorld!")
+print("Hello \rWorld!")
+
+# Raw String
+print(r'D:\User\asus\Coding')
+
+# Multiline String
+print("""
+Name : Bill Gates
+Company : Microsoft
+""")
+
+# Multiline String and RAW
+print(r"""
+Name : Steve Jobs
+Company : Microsoft
+Website : www.microsoft.com/
+""")
