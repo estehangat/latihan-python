52308d356423a4f6002ddbf667c6d82639c18d3a
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 19:59:19 +0700

    latihan logika

--- 7 - Logics Exercise.py
+++ 7 - Logics Exercise.py
@@ -1,15 +1,15 @@
 # Latihan logika menggunakan Comparison
 
-angka = float(input("Angka kurang dari 9 atau lebih dari 21 : "))
+angka = float(input("Angka kurang dari 9 atau lebih dari 21 : ")) # Input : 7
 
 # Lebih dari sama dengan
 lebih = (angka >= 21)
-print("Angka anda lebih dari 21", lebih)
+print("Angka anda lebih dari 21", lebih) # Output : Angka anda lebih dari 21 False
 
 # Kurang dari sama dengan
 kurang = (angka <= 9)
-print("Angka anda kurang dari 9", kurang)
+print("Angka anda kurang dari 9", kurang) # Output : Angka anda kurang dari 9 True
 
 # Irisan
 correct = lebih and kurang
-print("Angka anda beririsan", correct)
+print("Angka anda beririsan", correct) # Output : Angka anda beririsan False
