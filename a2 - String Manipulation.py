eca7adba1918335b1b900fd04a5ffb998053ac17
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 20:22:48 +0700

    manipulasi string

--- a2 - String Manipulation.py
+++ a2 - String Manipulation.py
@@ -4,39 +4,39 @@ awal = "Monkey"
 tengah = "D"
 akhir = "Luffy"
 lengkap = awal + " " + tengah + "'" + akhir
-print(lengkap)
+print(lengkap) # Output : Monkey D'Luffy
 
 # Menghitung Panjang String
 long = len(lengkap)
-print(long)
+print(long) # Output : 14
 
 # Mengecek Komponen String
 p = "p"
 stats = p in lengkap
-print(stats)
+print(stats) # Output : False
 
 p = "p"
 stats = p not in lengkap
-print(stats)
+print(stats) # Output : True
 
 # Mengulang String
-print("W"*9)
-print(9*"W")
+print("W"*9) # Output : WWWWWWWWW
+print(9*"W") # Output : WWWWWWWWW
 
 # Indexing
-print("Huruf ke-1", lengkap[0])
-print("Huruf ke-9", lengkap[10])
-print("Huruf ke-7-9", lengkap[6:8])
-print("Huruf ke-2,4,6,8,10", lengkap[1:9:2])
+print("Huruf ke-1", lengkap[0]) # Output : Huruf ke-1 M
+print("Huruf ke-9", lengkap[10]) # Output : Huruf ke-9 u
+print("Huruf ke-7-9", lengkap[6:8]) # Output : Huruf ke-7-9  D
+print("Huruf ke-2,4,6,8,10", lengkap[1:9:2]) # Output : Huruf ke-2,4,6,8,10 okyD
 
 # Ascii Code
 min = min(lengkap)
 max = max(lengkap)
 min1 = ord(min)
 max1 = ord(max)
-print("Ascii Code paling kecilnya yaitu", min, "dengan angka", min1)
-print("Ascii Code paling besarnya yaitu", max, "dengan angka", max1)
+print("Ascii Code paling kecilnya yaitu", min, "dengan angka", min1) # Output : Ascii Code paling kecilnya yaitu   dengan angka 32
+print("Ascii Code paling besarnya yaitu", max, "dengan angka", max1) # Output : Ascii Code paling besarnya yaitu y dengan angka 121
 
 # Counting
 full = lengkap.count('y')
-print("Jumlah huruf y pada", "'" + lengkap + "'", "adalah", full)
+print("Jumlah huruf y pada", "'" + lengkap + "'", "adalah", full) # Output : Jumlah huruf y pada 'Monkey D'Luffy' adalah 2
