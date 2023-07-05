98687aabdf66a876183be7a8d7d868b68915ed50
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 19:39:04 +0700

    komparasi

--- 5 - Comparison.py
+++ 5 - Comparison.py
@@ -7,27 +7,27 @@ b = 9
 
 # Lebih besar (>)
 c = a > b
-print(a, '>', b, c)
+print(a, '>', b, c) # Output : 21 > 9 True
 
 # Lebih kecil (<)
 c = a < b
-print(a, '<', b, c)
+print(a, '<', b, c) # Output : 21 < 9 False
 
 # Lebih besar sama dengan (>=)
 c = a >= b
-print(a, '>=', b, c)
+print(a, '>=', b, c) # Output : 21 >= 9 True
 
 # Lebih kecil sama dengan (<=)
 c = a <= b
-print(a, '<=', b, c)
+print(a, '<=', b, c) # Output : 21 <= 9 False
 
 # Sama dengan (==)
 c = a == b
-print(a, '==', b, c)
+print(a, '==', b, c) # Output : 21 == 9 False
 
 # Tidak sama dengan (!=)
 c = a != b
-print(a, '!=', b, c)
+print(a, '!=', b, c) # Output : 21 != 9 True
 
 ## Komparasi Object Identity
 
@@ -41,6 +41,12 @@ print(x is y) # returns False because x is not the same object as y, even if the
 print(x == y) # to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
 print(p) # True jika object adalah angka
 
+# Output :
+# True
+# False
+# True
+# False
+
 # is not
 x = ["apple", "banana"]
 y = ["apple", "banana"]
@@ -50,3 +56,9 @@ print(x is not z) # returns True because z is the same object as x
 print(x is not y) # returns False because x is not the same object as y, even if they have the same content (True jika object adalah angka)
 print(x == y) # to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
 print(p) # True jika object adalah angka
+
+# Output :
+# False
+# True
+# True
+# True
