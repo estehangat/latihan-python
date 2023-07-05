2c9ffcd0b394e860fb5bdf4943afdadada9090fe
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 19:34:59 +0700

    input data

--- 4 - Input Data.py
+++ 4 - Input Data.py
@@ -1,13 +1,13 @@
 # User Input
 
 # Data pasti string
-data = input("Masukkan : ")
-print("Karakter", data, "merupakan tipe data", type(data))
+data = input("Masukkan : ") # Input : 21
+print("Karakter", data, "merupakan tipe data", type(data)) # Output : Karakter 21 merupakan tipe data <class 'str'>
 
 # Change to int and float sama saja
-data_int = int(input("Masukkan : "))
-print("Karakter", data_int, "merupakan tipe data", type(data_int))
+data_int = int(input("Masukkan : ")) # Input : 21
+print("Karakter", data_int, "merupakan tipe data", type(data_int)) # Output : Karakter 21 merupakan tipe data <class 'int'>
 
 # Change to boolean
-data_bool = bool(int(input("Masukkan : ")))
-print("Karakter", data_bool, "merupakan tipe data", type(data_bool))
+data_bool = bool(int(input("Masukkan : "))) # Input : 21
+print("Karakter", data_bool, "merupakan tipe data", type(data_bool)) # Output : Karakter True merupakan tipe data <class 'bool'>
