fd63a6a7ca2c17bc5964331f277cc4174deb9db1
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 20:19:30 +0700

    string

--- a1 - String.py
+++ a1 - String.py
@@ -2,33 +2,33 @@
 
 # Single Quote
 user = 'Hello World!'
-print(user)
+print(user) # Output : Hello World!
 
 # Double Quote
 user = "Hello World!"
-print(user)
+print(user) # Output : Hello World!
 
 # Quotes
-print('"Hello World!"')
-print("'Hello World!'")
-print("It's 9 o'clock!")
+print('"Hello World!"') # Output : "Hello World!"
+print("'Hello World!'") # Output : 'Hello World!'
+print("It's 9 o'clock!") # Output : It's 9 o'clock!
 
 # Backslash
-print('It\'s 9 o\'clock!')
-print("C:\\User\\asus\\Program Files")
+print('It\'s 9 o\'clock!') # Output : It's 9 o'clock!
+print("C:\\User\\asus\\Program Files") # Output : C:\User\asus\Program Files
 
 # Tab
-print("Purwokerto \tJakarta")
+print("Purwokerto \tJakarta") # Output : Purwokerto      Jakarta
 
 # Backspace
-print("JKT \b48")
+print("JKT \b48") # Output : JKT48
 
 # Newline
-print("Hello \nWorld!")
-print("Hello \rWorld!")
+print("Hello \nWorld!") # Output : Hello \n World!
+print("Hello \rWorld!") # Output : World!
 
 # Raw String
-print(r'D:\User\asus\Coding')
+print(r'D:\User\asus\Coding') # Output : D:\User\asus\Coding
 
 # Multiline String
 print("""
@@ -36,9 +36,19 @@ Name : Bill Gates
 Company : Microsoft
 """)
 
+# Output :
+# Name : Bill Gates
+# Company : Microsoft
+
 # Multiline String and RAW
 print(r"""
 Name : Steve Jobs
 Company : Microsoft
 Website : www.microsoft.com/
 """)
+
+# Output :
+#
+# Name : Steve Jobs
+# Company : Microsoft
+# Website : www.microsoft.com/
