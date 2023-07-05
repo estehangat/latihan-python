b04195ef63502176eaa7ff2d5feea98b0e1569f3
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-05 20:13:43 +0700

    operator assignment

--- 9 - Operator Assignment.py
+++ 9 - Operator Assignment.py
@@ -3,43 +3,43 @@
 # Pertambahan
 a = 9
 a += 7
-print("Hasil pertambahan", a)
+print("Hasil pertambahan", a) # Output : Hasil pertambahan 16
 
 # Pengurangan
 a -= 1
-print("Hasil pengurangan", a)
+print("Hasil pengurangan", a) # Output : Hasil pengurangan 15
 
 # Perkalian
 a *= 4
-print("Hasil Perkalian", a)
+print("Hasil Perkalian", a) # Output : Hasil Perkalian 60
 
 # Pembagian
 a /= 3
-print("Hasil Pembagian", int(a))
+print("Hasil Pembagian", int(a)) # Output : Hasil Pembagian 20
 
 # Pembagian Sisa
 a %= 6
-print("Hasil sisanya", int(a))
+print("Hasil sisanya", int(a)) # Output : Hasil sisanya 2
 
 # Pembagian pembulatan ke bawah
 a //= 7
-print("Hasil pembulatan ke bawah", int(a))
+print("Hasil pembulatan ke bawah", int(a)) # Output : Hasil pembulatan ke bawah 0
 
 # Eksponen
 a **= 0
-print("Hasil eksponen", int(a))
+print("Hasil eksponen", int(a)) # Output : Hasil eksponen 1
 
 # OR
 q = True
 q |= False
-print("Maka akan menjadi", q)
+print("Maka akan menjadi", q) # Output : Maka akan menjadi True
 
 # AND
 q = True
 q &= False
-print("Maka akan menjadi", q)
+print("Maka akan menjadi", q) # Output : Maka akan menjadi False
 
 # XOR
 q = True
 q ^= True
-print("Maka akan menjadi", q)
+print("Maka akan menjadi", q) # Output : Maka akan menjadi False
