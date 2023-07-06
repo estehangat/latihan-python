7ddd31800c2a2737aad9afaf440fc3faee5090f8
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-06 14:03:06 +0700

    if dan else

---
+++ a6 - If and Else.py
@@ -0,0 +1,18 @@
+# If dan Else
+
+# If Inline
+user = input("Who? ") # Input : Maeng
+if user == "Maeng" : print("Anda Benar") # Output : Anda Benar
+print(f"Arigatou, {user}") # Output : Arigatou, Maeng
+
+# If Indentation
+if user == "Maeng" :
+    print("You're amazing") # Output : You're amazing
+    print("You're doing well") # Output : You're doing well
+print(f"Arigatou, {user}") # Output : Arigatou, Maeng
+
+# Else
+if user == "Maeng" :
+    print("You're my G") # Output : You're my G
+else :
+    print("Not my G") # Output : Not my G
