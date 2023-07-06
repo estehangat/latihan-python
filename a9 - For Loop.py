5ad224e2ee38bb173dfd09f74c770a1ad8890ac1
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-06 20:07:20 +0700

    loooopp

---
+++ a9 - For Loop.py
@@ -0,0 +1,27 @@
+# For Loop (Perulangan)
+
+# Loop List
+angka = [2,3,5,7,11]
+for i in angka:
+    print(f"Number {i}")
+    # Output :
+    # Number 2
+    # Number 3
+    # Number 5
+    # Number 7
+    # Number 11
+
+# Range
+for i in range(5):
+    print(i)
+    # Output :
+    # Number 0-4
+
+for i in range(9,22):
+    print(i)
+    # Output :
+    # Number 9-21
+
+# Loop String
+for i in "Maeng":
+    print(i)
