a91657db04aa60ad251ec01127ea847809c3a147
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-06 23:02:34 +0700

    break

---
+++ b3 - Break.py
@@ -0,0 +1,12 @@
+# Break
+
+innit = int(input("Ketik angka \t: ")) # Input : 21
+user = 0
+while True:
+    user += 1
+    print(f"Just my homie {user}") # Output : Just my homie 1-21
+
+    if user == innit:
+        print("That's my G") # Output : That's my G
+        break # Stop di Input
+    print("TF G") # Output : TF G
