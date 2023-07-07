ae57a4ec68eb74d9a6f9a51ab551401d49cea266
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-07 15:36:09 +0700

    latihan looping

---
+++ b4 - Looping Exercise.py
@@ -0,0 +1,52 @@
+# Latihan Perulangan
+
+user = 21
+## For
+count = 1
+for i in range(9):
+    print("$"*count) # Output : $
+    count += 1
+
+print("\n")
+
+## While
+count = 1
+while True:
+    print("$"*count) # Output : $
+    count += 1
+    if count > user:
+        break
+
+## Continue
+user = 10
+count = 1
+while True:
+    if (count%2):
+    # Print jika ganjil
+        print("$"*count) # Output : $
+        count += 1
+    else: 
+    # Kembali ke atas jika ganjil
+        count += 1
+        continue
+    if count > user:
+    # Break jika count melebihi user
+        break
+
+## Belah Ketupat
+user = 10
+count = 1
+space = int(user/2)
+while True:
+    if (count%2):
+    # Print jika ganjil
+        print(" "*space, "+"*count) # Output : +
+        space -= 1
+        count += 1
+    else: 
+    # Kembali ke atas jika ganjil
+        count += 1
+        continue
+    if count > user:
+    # Break jika count melebihi user
+        break
