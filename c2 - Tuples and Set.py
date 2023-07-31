9f22dfc5559be58c0886c79322b927ff690b6eb6
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-31 22:52:13 +0700

    tuples sets

--- c2 - Tuples and Set.py
+++ c2 - Tuples and Set.py
@@ -2,13 +2,13 @@
 
 list = [1,2,3,4,5]
 tuples = (1,2,3,4,5)
-print(list)
-print(tuples)
-print(tuples[2])
+print(list) # Output : [1, 2, 3, 4, 5]
+print(tuples) # Output : (1, 2, 3, 4, 5)
+print(tuples[2]) # Output : 3
 # Data Tuples tidak bisa berubah, konstan
 
 # Sets
 
 sets = {1,2,3,4,5}
-print(sets)
+print(sets) # Output : {1, 2, 3, 4, 5}
 # Data Sets tidak punya index (Himpunan)
