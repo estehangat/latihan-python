6ef712209cf6d5fd347338dda2e4287fee4c6467
estehangat <96716034+estehangat@users.noreply.github.com>
2023-07-31 22:31:30 +0700

    tuples sets

---
+++ c2 - Tuples and Set.py
@@ -0,0 +1,14 @@
+# Kurung Siku
+
+list = [1,2,3,4,5]
+tuples = (1,2,3,4,5)
+print(list)
+print(tuples)
+print(tuples[2])
+# Data Tuples tidak bisa berubah, konstan
+
+# Sets
+
+sets = {1,2,3,4,5}
+print(sets)
+# Data Sets tidak punya index (Himpunan)
