69c4876014c873f64874ffe715366d35e16d14bd
estehangat <96716034+estehangat@users.noreply.github.com>
2023-08-03 15:06:11 +0700

    dictionary exercise

--- c8 - Dictionary Exercise.py
+++ c8 - Dictionary Exercise.py
@@ -1,5 +1,7 @@
 import datetime
 import os
+import string
+import random
 
 # Template Mahasiswa
 template = {
@@ -10,18 +12,39 @@ template = {
 }
 
 data = {}
-os.system("cls") # Untuk Windows, Untuk Menghilangkan Tulisan Path System
 
-print(f"{'ARSIP DATA':^16}\n {'MAHASISWA':^16}")
-print("="*16)
+while True:
+    os.system("cls") # Untuk Windows, Untuk Menghilangkan Tulisan Path System
 
-mahasiswa = dict.fromkeys(template.keys())
-print(mahasiswa) # Output : {'Nama': None, 'NIM': None, 'Lahir': None, 'SKS': None}
-mahasiswa['Nama'] = input("Nama \t\t: ")
-mahasiswa['NIM'] = input("NIM \t\t: ")
-TAHUN = int(input("Tahun (yyyy) \t: "))
-BULAN = int(input("Bulan (mm) \t: "))
-TANGGAL = int(input("Tanggal (dd) \t: "))
-mahasiswa['Lahir'] = datetime.datetime(TAHUN,BULAN,TANGGAL)
-mahasiswa['SKS'] = input("SKS \t\t: ")
-print(mahasiswa) # Output : {'Nama': None, 'NIM': None, 'Lahir': None, 'SKS': None}
+    print(f"{'ARSIP DATA':^16}\n {'MAHASISWA':^16}")
+    print("="*16)
+
+    mahasiswa = dict.fromkeys(template.keys())
+    mahasiswa['Nama'] = input("Nama \t\t: ")
+    mahasiswa['NIM'] = input("NIM \t\t: ")
+    TAHUN = int(input("Tahun (yyyy) \t: "))
+    BULAN = int(input("Bulan (mm) \t: "))
+    TANGGAL = int(input("Tanggal (dd) \t: "))
+    mahasiswa['Lahir'] = datetime.datetime(TAHUN,BULAN,TANGGAL)
+    mahasiswa['SKS'] = input("SKS \t\t: ")
+
+    KEY = ''.join(random.choice(string.ascii_uppercase) for i in range(4))
+    data.update({KEY:mahasiswa})
+
+    print('\n')
+    print(f"| {'KEY':^6} | {'NAMA':^18} | {'NIM':^12} | {'LAHIR':^10} | {'SKS':^8} |")
+    print("="*70)
+
+    for key in data:
+        KEY = key
+        NAMA = data[KEY]['Nama']
+        NIM = data[KEY]['NIM']
+        LAHIR = data[KEY]['Lahir'].strftime("%x")
+        SKS = data[KEY]['SKS']
+        print(f"| {KEY:<6} | {NAMA:<18} | {NIM:<12} | {LAHIR:<10} | {SKS:<8} |")
+
+    print('\n')
+
+    done = input("Apakah sudah selesai? (y/n) ") # Output : Apakah sudah selesai? (y/n)
+    if done == 'y':
+        break
